dec3dfb4dd3e460f9b8181987a58df8761130d05
liqx <s.vlasiuk@dev-branch.com>
2024-09-24 08:14:09 +0000

    Redmine#8580: reconfigured script with set command for better error handling

--- docker_under_portainer/backup.sh
+++ docker_under_portainer/backup.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+# Enable options for better error handling
+set -euo pipefail
+
 # Configuration
 BACKUP_DIR="/home/backups/files"
-LOG_FILE="/var/log/cronjob/backup.log"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 RETENTION_DAYS=5
 TEMP_LOG=$(mktemp)
 
 # Create folder for backup
 NAME_DIR="$BACKUP_DIR/liqx_$DATE"
-mkdir -p "$NAME_DIR"
+
+if mkdir -p "$NAME_DIR"; then
+    echo "$DATE - Created directory $NAME_DIR"
+else
+    echo "$DATE - Error: Cannot create directory $NAME_DIR, please check permissions and Readme file"
+    exit 1
+fi
 
 # Backup Database
 if drush sql:dump --result-file=$NAME_DIR/backup-$DATE.sql --gzip --skip-tables-list=cache*; then
     echo "$DATE - Database backup created at $NAME_DIR/backup-$DATE.sql.gz"
 else
     echo "$DATE - Error: Database backup failed."
+    exit 1
 fi
 
 # Backup Website Files
@@ -23,6 +32,7 @@ if tar -czvf "$NAME_DIR/files_backup_$DATE.tar.gz" /var/www/html/web/sites/defau
     echo "$DATE - Website files backup created at $NAME_DIR/files_backup_$DATE.tar.gz"
 else
     echo "$DATE - Error: Website files backup failed."
+    exit 1
 fi
 
 # Clean up old backups
