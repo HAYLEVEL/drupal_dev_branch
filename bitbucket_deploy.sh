6c1acdb3ce3d41b485ca3e0aa99b93b36e2fbb1f
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-01 09:53:13 +0000

    test

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -9,8 +9,8 @@ NODE_CONTAINER=$5
 BITBUCKET_COMMIT=$6
 
 # Connect to remote
-ssh $REMOTE_USER@$REMOTE_HOST << EOF
-CURRENT_COMMIT_HASH=$(docker exec $ENVIRONMENT_CONTAINER sh -c 'git rev-parse HEAD')
+ssh $REMOTE_USER@$REMOTE_HOST << 'EOF'
+CURRENT_COMMIT_HASH=\$(docker exec $ENVIRONMENT_CONTAINER sh -c 'git rev-parse HEAD')
 
 deploy_func() {( set -e  # Exit if any command within the function fails
     docker exec p$ENVIRONMENT_CONTAINER sh -c 'git config --global --add safe.directory /var/www/html'
@@ -26,7 +26,7 @@ deploy_func() {( set -e  # Exit if any command within the function fails
     sleep 20
     docker exec $ENVIRONMENT_CONTAINER sh -c 'composer install --optimize-autoloader'
     docker exec $ENVIRONMENT_CONTAINER sh -c 'vendor/bin/drush deploy -y -v'
-  )}
+)}
 
 rollback_func() {( set -e  # Exit if any command within the function fails
     echo "Reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
@@ -35,26 +35,17 @@ rollback_func() {( set -e  # Exit if any command within the function fails
     docker start $NODE_CONTAINER
     sleep 20
     docker exec $ENVIRONMENT_CONTAINER sh -c 'vendor/bin/drush deploy -y -v'
-  )}
+)}
+
 ########################################################################
-  deploy_func
-  DEPLOY_EXIT_CODE=\$?
+deploy_func || {
+  echo "Deployment failed, starting rollback to commit hash \$CURRENT_COMMIT_HASH"
+  rollback_func || {
+    echo "Rollback failed----------------------------------------------"
+    exit 1
+  }
+  exit 1
+}
 
-  if [ \$DEPLOY_EXIT_CODE -ne 0 ]; then
-    echo "Deployment failed with exit code \$DEPLOY_EXIT_CODE"
-    echo "Start reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
-    rollback_func
-    ROLLBACK_EXIT_CODE=\$?
-    if [ \$ROLLBACK_EXIT_CODE -ne 0 ]; then
-      echo "Rollback failed with exit code \$ROLLBACK_EXIT_CODE"
-      exit \$ROLLBACK_EXIT_CODE
-    else
-      echo "Rollback succeeded but deployment failed with exit code \$DEPLOY_EXIT_CODE"
-      exit \$DEPLOY_EXIT_CODE
-    fi
-  else
-    echo "Deployment succeeded-----------------------------------------"
-    exit 0
-  fi
-
+echo "Deployment succeeded---------------------------------------------"
 EOF
