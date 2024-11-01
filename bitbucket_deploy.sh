0b6912e302114ad7e8053409e10b2dfcd37ff6a4
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-01 13:25:58 +0000

    dsa

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -32,7 +32,7 @@ deploy_func() {( set -e  # Exit if any command within the function fails
 
 rollback_func() {( set -e  # Exit if any command within the function fails
     echo "Reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
-    echo $CURRENT_COMMIT_HASH
+    echo \$CURRENT_COMMIT_HASH
     git checkout $CURRENT_COMMIT_HASH
     docker exec $ENVIRONMENT_CONTAINER sh -c 'composer install --optimize-autoloader'
     docker start $NODE_CONTAINER
