fb4c7f9314690edf4a70b3519ffd212545304403
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-01 12:46:42 +0000

    dsa

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -16,6 +16,7 @@ ssh $REMOTE_USER@$REMOTE_HOST << EOF
   cd "$SITE_DIR"
 
 deploy_func() {( set -e  # Exit if any command within the function fails
+    cd $SITE_DIR
     git checkout origin/$BITBUCKET_BRANCH
     git pull origin $BITBUCKET_BRANCH
 
