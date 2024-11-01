87aa3df17d553fb4c158fcb19c44c20c7a71b086
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-01 10:12:54 +0000

    test

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -10,8 +10,8 @@ BITBUCKET_COMMIT=$6
 
 # Connect to remote
 ssh $REMOTE_USER@$REMOTE_HOST << EOF
-CURRENT_COMMIT_HASH=$(docker exec $ENVIRONMENT_CONTAINER sh -c 'git rev-parse HEAD')
-echo "$CURRENT_COMMIT_HASH"
+echo $(docker exec $ENVIRONMENT_CONTAINER sh -c 'git rev-parse HEAD')
+
 
 deploy_func() {( set -e  # Exit if any command within the function fails
     docker exec p$ENVIRONMENT_CONTAINER sh -c 'git config --global --add safe.directory /var/www/html'
