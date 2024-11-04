69467dd130bae2e19aa2eecc18a470ef23784165
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-04 09:10:07 +0000

    test

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -12,17 +12,20 @@ SITE_DIR=$7
 # Connect to remote
 ssh $REMOTE_USER@$REMOTE_HOST << EOF
 CURRENT_COMMIT_HASH=\$(docker exec $ENVIRONMENT_CONTAINER sh -c 'git rev-parse HEAD')
-cd $SITE_DIR
+
+red() { echo -e "\033[0;31m\$1\033[0m" }
+green() { echo -e "\033[0;32m\$1\033[0m" }
+yellow() { echo -e "\033[1;33m\$1\033[0m" }
 
 deploy_func() {( set -e  # Exit if any command within the function fails
     git checkout origin/$BITBUCKET_BRANCH
     git pull origin $BITBUCKET_BRANCH
 
-    echo "Starting database backup--------------------------------------"
+    yellow "Starting database backup--------------------------------------"
     docker exec $ENVIRONMENT_CONTAINER sh -c 'vendor/bin/drush sql:dump --result-file=/var/www/html/back_sql/backup.sql --gzip --skip-tables-list=cache*'
     docker cp $ENVIRONMENT_CONTAINER:/var/www/html/back_sql/backup.sql.gz ~/back_sql/$DEPLOYMENT_ENVIRONMENT/backup_$BITBUCKET_COMMIT.sql.gz
 
-    echo "Deploy to docker stack----------------------------------------"
+    yellow "Deploy to docker stack----------------------------------------"
     docker start $NODE_CONTAINER
     sleep 20
     docker exec $ENVIRONMENT_CONTAINER sh -c 'composer install --optimize-autoloader'
@@ -30,7 +33,7 @@ deploy_func() {( set -e  # Exit if any command within the function fails
 )}
 
 rollback_func() {( set -e  # Exit if any command within the function fails
-    echo "Reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
+    yellow "Reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
     git checkout \$CURRENT_COMMIT_HASH
     docker exec $ENVIRONMENT_CONTAINER sh -c 'composer install --optimize-autoloader'
     docker start $NODE_CONTAINER
@@ -39,23 +42,23 @@ rollback_func() {( set -e  # Exit if any command within the function fails
 )}
 
 ########################################################################
-cd "$SITE_DIR"
+cd "\$SITE_DIR"
 deploy_func
 DEPLOY_EXIT_CODE=\$?
 if [ \$DEPLOY_EXIT_CODE -ne 0 ]; then
-    echo "Deployment failed with exit code \$DEPLOY_EXIT_CODE"
-    echo "Start reverting Drupal site to commit hash \$CURRENT_COMMIT_HASH"
+    red "Deployment failed with exit code \$DEPLOY_EXIT_CODE"
     rollback_func
     ROLLBACK_EXIT_CODE=\$?
     if [ \$ROLLBACK_EXIT_CODE -ne 0 ]; then
-        echo "Rollback failed with exit code \$ROLLBACK_EXIT_CODE"
+        red "Rollback failed with exit code \$ROLLBACK_EXIT_CODE"
         exit \$ROLLBACK_EXIT_CODE
     else
-        echo "Rollback succeeded but deployment failed with exit code \$DEPLOY_EXIT_CODE"
+        yellow "Rollback succeeded but deployment failed with exit code \$DEPLOY_EXIT_CODE"
         exit \$DEPLOY_EXIT_CODE
     fi
 else
-    echo "Deployment succeeded-----------------------------------------"
+    green "Deployment succeeded-----------------------------------------"
     exit 0
 fi
 EOF
+
