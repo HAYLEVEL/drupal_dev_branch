477d20b3f902d2dc2cb8d702510e07bd6038ea33
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-06 08:06:59 +0000

    Redmine#8728: Now starts backup before starting a deploy function (git commands)

--- bitbucket_deploy.sh
+++ bitbucket_deploy.sh
@@ -29,12 +29,12 @@ disable_maintenance_mode() {( set -e
 )}
 
 deploy_func() {( set -e  # Exit if any command within the function fails
-    git checkout $BITBUCKET_BRANCH
-    git pull origin $BITBUCKET_BRANCH
-
     yellow "Starting database backup--------------------------------------"
     docker exec $ENVIRONMENT_CONTAINER sh -c 'vendor/bin/drush sql:dump --result-file=/tmp/backup.sql --gzip --skip-tables-list=cache*'
     docker cp $ENVIRONMENT_CONTAINER:/tmp/backup.sql.gz ~/back_sql/$DEPLOYMENT_ENVIRONMENT/backup_$BITBUCKET_COMMIT.sql.gz
+
+    git checkout $BITBUCKET_BRANCH
+    git pull origin $BITBUCKET_BRANCH
 
     yellow "Deploy to docker stack----------------------------------------"
     docker start -i $NODE_CONTAINER
