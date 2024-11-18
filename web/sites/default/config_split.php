af08151e6cca06d4916eb30ebffd4670097f18f9
liqx_drupalist <s.vlasiuk@dev-branch.com>
2024-11-18 08:57:22 +0000

    Redmine#8797: Added environment to .env files. Configured d4d stacks to use DRUPAL ENV from .env files. Fixed Drupal coding standard in config_split.php file. Added required modules to split configuration.

--- web/sites/default/config_split.php
+++ web/sites/default/config_split.php
@@ -1,9 +1,18 @@
 <?php
+
+/**
+ * @file
+ * Configuration split settings for different environments.
+ *
+ * This file determines which configuration split is active based
+ * on the `DRUPAL_ENV` environment variable.
+ */
+
 if (getenv('DRUPAL_ENV') === 'production') {
   $config['config_split.config_split.config_development']['status'] = FALSE;
   $config['config_split.config_split.config_production']['status'] = TRUE;
 }
-elseif(getenv('DRUPAL_ENV') === 'development') {
+elseif (getenv('DRUPAL_ENV') === 'development') {
   $config['config_split.config_split.config_development']['status'] = TRUE;
   $config['config_split.config_split.config_production']['status'] = FALSE;
 }
